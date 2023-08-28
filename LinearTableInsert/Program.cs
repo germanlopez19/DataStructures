1aef6208703d5dbccd4d31a8270b6b72cad7cc9d
German Lopez <german.lopez19@ymail.com>
2023-08-28 00:41:25 -0600

    Add Linear Tabler Insert

---
+++ LinearTableInsert/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinearTableInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 90, 70, 50, 80, 60, 85 };
+            array = TestOneArrayInsert.Insert(array, 75, 2);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
