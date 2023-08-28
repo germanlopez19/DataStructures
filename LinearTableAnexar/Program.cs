e9aee3521e29a6fe888ba16a6acf0b9cc7b1e985
German Lopez <german.lopez19@ymail.com>
2023-08-27 23:45:15 -0600

    Add Linear Table Anexar

--- LinearTableAnexar/Program.cs
+++ LinearTableAnexar/Program.cs
@@ -6,7 +6,14 @@ namespace LinearTableAnexar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 90, 70, 50, 80, 60, 85 };
+            array = TestOneArrayAppend.Add(array, 75);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ LinearTableAnexar/TestOneArrayAppend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LinearTableAnexar
+{
+    public class TestOneArrayAppend
+    {
+        public static int[] Add(int[] array, int value)
+        {
+            int[] tempArray = new int[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[array.Length] = value;
+            return tempArray;
+        }
+    }
+}
