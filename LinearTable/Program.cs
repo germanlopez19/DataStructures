2103a06f9e99ea7223a82fb2a6c9a4a7786fbc70
German Lopez <german.lopez19@ymail.com>
2023-08-27 23:03:28 -0600

    first commit

---
+++ LinearTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinearTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 90, 70, 50, 80, 60, 85 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinearTableAnexar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinearTableAnexar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
